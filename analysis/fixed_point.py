37fc8ce4023e239e3c9df5ef7a02133bab7390f1
eShuttleworth <14313854+eShuttleworth@users.noreply.github.com>
2025-07-23 17:26:11 -0500

    Clarify worklist algorithm

--- analysis/fixed_point.py
+++ analysis/fixed_point.py
@@ -1,10 +1,12 @@
 """This module implements the fixed point algorithm."""
+from collections import deque
+
 from .constraint_table import constraint_table
 from .reaching_definitions_taint import ReachingDefinitionsTaintAnalysis
 
 
-class FixedPointAnalysis():
-    """Run the fix point analysis."""
+class FixedPointAnalysis:
+    """Run the fix point analysis using a worklist algorithm."""
 
     def __init__(self, cfg):
         """Fixed point analysis.
@@ -15,19 +17,22 @@ class FixedPointAnalysis():
         self.cfg = cfg
 
     def fixpoint_runner(self):
-        """Work list algorithm that runs the fixpoint algorithm."""
-        q = self.cfg.nodes
+        """Iteratively update nodes until a fixpoint is reached.
 
-        while q != []:
-            x_i = constraint_table[q[0]]  # x_i = q[0].old_constraint
-            self.analysis.fixpointmethod(q[0])  # y = F_i(x_1, ..., x_n);
-            y = constraint_table[q[0]]  # y = q[0].new_constraint
+        A deque is used for the worklist to avoid the costly list slicing that
+        previously led to excessive memory usage on large graphs.
+        """
+        worklist = deque(self.cfg.nodes)
 
-            if y != x_i:
-                for node in self.analysis.dep(q[0]):  # for (v in dep(v_i))
-                    q.append(node)  # q.append(v):
-                constraint_table[q[0]] = y  # q[0].old_constraint = q[0].new_constraint # x_i = y
-            q = q[1:]  # q = q.tail()  # The list minus the head
+        while worklist:
+            node = worklist.popleft()
+            old_constraint = constraint_table[node]
+            self.analysis.fixpointmethod(node)
+            new_constraint = constraint_table[node]
+
+            if new_constraint != old_constraint:
+                for dep_node in self.analysis.dep(node):
+                    worklist.append(dep_node)
 
 
 def analyse(cfg_list):
